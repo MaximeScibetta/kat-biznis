844906b373f3c30cf07da6ddfd2db8cd8b73cbe7
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-02-09 11:53:50 +0100

    Gulp task for pug files

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@
  * coded by maxime@flatLand !
  * started at 09/02/2017
  */
-var gulp = require( "gulp" );
-    image = require( "gulp-image" );
+var gulp = require( "gulp" ),
+    image = require( "gulp-image" ),
+    pug = require( "gulp-pug" );
 // --- Task for images
 gulp.task( "images", function() {
     gulp.src( "src/images/**" )
@@ -16,7 +17,11 @@ gulp.task( "images", function() {
 // --- Task for styles 
 
 // --- Task for pug
-
+gulp.task( "html", function() {
+    gulp.src( "src/pug/**/*.pug" )
+        .pipe( pug( {} ) )
+        .pipe( gulp.dest( "." ) );
+} );
 // --- Task for js
 
 // --- Watch tasks
