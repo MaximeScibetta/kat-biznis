8994df79088d9a51c535f57f458e9d5718acfdaa
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-02-09 12:08:36 +0100

    Gulp task for scss files

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@
  * coded by maxime@flatLand !
  * started at 09/02/2017
  */
-var gulp = require( "gulp" ),
+var gulp = require( "gulp" ),yar
     image = require( "gulp-image" ),
-    pug = require( "gulp-pug" );
+    pug = require( "gulp-pug" ),
+    sass = require( "gulp-sass" ),
+    autoprefixer = require( "gulp-autoprefixer" ),
+    csso = require ( "gulp-csso" );
 // --- Task for images
 gulp.task( "images", function() {
     gulp.src( "src/images/**" )
@@ -15,7 +18,13 @@ gulp.task( "images", function() {
         .pipe( gulp.dest( "assets/images" ) );
 } );
 // --- Task for styles 
-
+gulp.task( "css", function() {
+    gulp.src( "src/sass/**/*.scss" )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( autoprefixer() )
+        .pipe( csso() )
+        .pipe( gulp.dest( "assets/css" ) );
+} );
 // --- Task for pug
 gulp.task( "html", function() {
     gulp.src( "src/pug/**/*.pug" )
