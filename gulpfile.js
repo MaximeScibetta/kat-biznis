f971f72e038225d89b8e9eaaa5f39644be60f285
Maxime Scibetta <maxime.scibetta@outlook.com>
2017-02-09 11:44:25 +0100

    Gulp task for images

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@
  * started at 09/02/2017
  */
 var gulp = require( "gulp" );
-
+    image = require( "gulp-image" );
 // --- Task for images
-
+gulp.task( "images", function() {
+    gulp.src( "src/images/**" )
+        .pipe( image() )
+        .pipe( gulp.dest( "assets/images" ) );
+} );
 // --- Task for styles 
 
 // --- Task for pug
